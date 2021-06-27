17bbe841147218522eb26cf7a2d3e8303f82bea8
Nikita Shamgunov <nikita@a000738.lan>
2021-06-27 00:05:14 -0700

    3

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Nikita2!</a>
+          Welcome to <a href="https://nextjs.org">Nikita3!</a>
         </h1>
 
         <p className={styles.description}>
